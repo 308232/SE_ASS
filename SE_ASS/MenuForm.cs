9de537aea90442251e5783a37ea91d6b98ca4478
308232 <308232@gllm.ac.uk>
2021-03-02 14:39:13 +0000

    Deletetd the test form and created the Menu and the Main forms. Also added the username and pasword code.

---
+++ SE_ASS/MenuForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SE_ASS
+{
+    
+
+    public partial class MenuForm : Form
+    {
+       public string userRights;
+
+        public MenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmitUserNamePassword_Click(object sender, EventArgs e)
+        {
+            if ((txtUserName.Text == "Owner") &&(txtPassword.Text=="asdf"))
+            {
+                userRights = "Owner";
+
+                MainForm MainForm = new MainForm();
+                this.Hide();
+                MainForm.Show();
+            }
+            else if ((txtUserName.Text == "Admin") && (txtPassword.Text == "asd"))
+            {
+                userRights = "Admin";
+
+                MainForm MainForm = new MainForm();
+                this.Hide();
+                MainForm.Show();
+            }
+            else if ((txtUserName.Text == "LC") && (txtPassword.Text == "asdf"))
+            {
+                userRights = "LC";
+
+                MainForm MainForm = new MainForm();
+                this.Hide();
+                MainForm.Show();
+            }
+            else if ((txtUserName.Text == "Couriers") && (txtPassword.Text == "as"))
+            {
+                userRights = "Couriers";
+
+                MainForm MainForm = new MainForm();
+                this.Hide();
+                MainForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("You Have entered incorect username or password. Please try again");
+                txtUserName.Clear();
+                txtPassword.Clear();
+            }
+
+
+
+
+
+        }
+    }
+}

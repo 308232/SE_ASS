e471089b5b7fbaed9ae95606dd4da70c8325ad13
308232 <308232@gllm.ac.uk>
2021-03-10 22:16:20 +0000

    Added buttons and datagrid view in order to obtain the reports needed for the program.

--- SE_ASS/MainForm.Designer.cs
+++ SE_ASS/MainForm.Designer.cs
@@ -35,6 +35,17 @@ namespace SE_ASS
             this.dataGridViewAdmin = new System.Windows.Forms.DataGridView();
             this.comboboxWhichday = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.txtboxStartDate = new System.Windows.Forms.TextBox();
+            this.txtboxEndDate = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAllCAss = new System.Windows.Forms.Button();
+            this.txtboxCourierID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtboxCourierdate = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAdmin)).BeginInit();
             this.SuspendLayout();
             // 
@@ -51,7 +62,7 @@ namespace SE_ASS
             // 
             // btnCreateNewClientContract
             // 
-            this.btnCreateNewClientContract.Location = new System.Drawing.Point(97, 118);
+            this.btnCreateNewClientContract.Location = new System.Drawing.Point(12, 276);
             this.btnCreateNewClientContract.Name = "btnCreateNewClientContract";
             this.btnCreateNewClientContract.Size = new System.Drawing.Size(107, 59);
             this.btnCreateNewClientContract.TabIndex = 1;
@@ -61,20 +72,22 @@ namespace SE_ASS
             // 
             // btnShowAllAssForACourierForADAY
             // 
-            this.btnShowAllAssForACourierForADAY.Location = new System.Drawing.Point(361, 118);
+            this.btnShowAllAssForACourierForADAY.BackColor = System.Drawing.SystemColors.Menu;
+            this.btnShowAllAssForACourierForADAY.ForeColor = System.Drawing.Color.Crimson;
+            this.btnShowAllAssForACourierForADAY.Location = new System.Drawing.Point(176, 132);
             this.btnShowAllAssForACourierForADAY.Name = "btnShowAllAssForACourierForADAY";
-            this.btnShowAllAssForACourierForADAY.Size = new System.Drawing.Size(117, 80);
+            this.btnShowAllAssForACourierForADAY.Size = new System.Drawing.Size(110, 80);
             this.btnShowAllAssForACourierForADAY.TabIndex = 2;
-            this.btnShowAllAssForACourierForADAY.Text = "Show All Ass for a choosen day ";
-            this.btnShowAllAssForACourierForADAY.UseVisualStyleBackColor = true;
+            this.btnShowAllAssForACourierForADAY.Text = "Obtain a report for all assignments \r\nfor all \r\ncouriers for the current month";
+            this.btnShowAllAssForACourierForADAY.UseVisualStyleBackColor = false;
             this.btnShowAllAssForACourierForADAY.Click += new System.EventHandler(this.btnShowAllAssForACourierForADAY_Click);
             // 
             // dataGridViewAdmin
             // 
             this.dataGridViewAdmin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewAdmin.Location = new System.Drawing.Point(351, 248);
+            this.dataGridViewAdmin.Location = new System.Drawing.Point(176, 248);
             this.dataGridViewAdmin.Name = "dataGridViewAdmin";
-            this.dataGridViewAdmin.Size = new System.Drawing.Size(498, 168);
+            this.dataGridViewAdmin.Size = new System.Drawing.Size(673, 168);
             this.dataGridViewAdmin.TabIndex = 3;
             // 
             // comboboxWhichday
@@ -87,20 +100,131 @@ namespace SE_ASS
             "Thursday",
             "Friday",
             "Saturday"});
-            this.comboboxWhichday.Location = new System.Drawing.Point(661, 189);
+            this.comboboxWhichday.Location = new System.Drawing.Point(802, 12);
             this.comboboxWhichday.Name = "comboboxWhichday";
             this.comboboxWhichday.Size = new System.Drawing.Size(132, 21);
             this.comboboxWhichday.TabIndex = 4;
+            this.comboboxWhichday.Visible = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(657, 139);
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(7, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(265, 38);
+            this.label1.Size = new System.Drawing.Size(220, 95);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Select a date from the dropdown box \r\nand then press the button \r\n";
+            this.label1.Text = "ENTER DATES IN THE\r\nTEXT BOXES TO SEARCH \r\nBETWEEN A PERIOD OF DAYS\r\nDATE FORMAT " +
+    "IS: YYYY-MM-DD\r\n\r\n";
+            // 
+            // txtboxStartDate
+            // 
+            this.txtboxStartDate.Location = new System.Drawing.Point(212, 218);
+            this.txtboxStartDate.Name = "txtboxStartDate";
+            this.txtboxStartDate.Size = new System.Drawing.Size(100, 20);
+            this.txtboxStartDate.TabIndex = 6;
+            // 
+            // txtboxEndDate
+            // 
+            this.txtboxEndDate.Location = new System.Drawing.Point(408, 218);
+            this.txtboxEndDate.Name = "txtboxEndDate";
+            this.txtboxEndDate.Size = new System.Drawing.Size(100, 20);
+            this.txtboxEndDate.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Info;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(135, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "StartDate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Info;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(335, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "EndDate";
+            // 
+            // btnAllCAss
+            // 
+            this.btnAllCAss.BackColor = System.Drawing.SystemColors.Menu;
+            this.btnAllCAss.ForeColor = System.Drawing.Color.Crimson;
+            this.btnAllCAss.Location = new System.Drawing.Point(719, 122);
+            this.btnAllCAss.Name = "btnAllCAss";
+            this.btnAllCAss.Size = new System.Drawing.Size(100, 73);
+            this.btnAllCAss.TabIndex = 10;
+            this.btnAllCAss.Text = "Obtain a report for all\r\n assignments for a\r\n courier for that day";
+            this.btnAllCAss.UseVisualStyleBackColor = false;
+            this.btnAllCAss.Click += new System.EventHandler(this.btnAllCAss_Click);
+            // 
+            // txtboxCourierID
+            // 
+            this.txtboxCourierID.Location = new System.Drawing.Point(605, 218);
+            this.txtboxCourierID.Name = "txtboxCourierID";
+            this.txtboxCourierID.Size = new System.Drawing.Size(100, 20);
+            this.txtboxCourierID.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Info;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label4.Location = new System.Drawing.Point(526, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 48);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Enter Courier ID AND Date \r\nin the \r\ntext box to search ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Info;
+            this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(526, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "CourierID:";
+            // 
+            // txtboxCourierdate
+            // 
+            this.txtboxCourierdate.Location = new System.Drawing.Point(783, 218);
+            this.txtboxCourierdate.Name = "txtboxCourierdate";
+            this.txtboxCourierdate.Size = new System.Drawing.Size(100, 20);
+            this.txtboxCourierdate.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Info;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(731, 221);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 16);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Date:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(326, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 80);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Obtain a report for all assignments \r\nshowing contracts and non-contract\r\n client" +
+    "s for a given month";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // MainForm
             // 
@@ -108,6 +232,17 @@ namespace SE_ASS
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(934, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtboxCourierdate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtboxCourierID);
+            this.Controls.Add(this.btnAllCAss);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtboxEndDate);
+            this.Controls.Add(this.txtboxStartDate);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboboxWhichday);
             this.Controls.Add(this.dataGridViewAdmin);
@@ -130,5 +265,16 @@ namespace SE_ASS
         private System.Windows.Forms.DataGridView dataGridViewAdmin;
         private System.Windows.Forms.ComboBox comboboxWhichday;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtboxStartDate;
+        private System.Windows.Forms.TextBox txtboxEndDate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAllCAss;
+        private System.Windows.Forms.TextBox txtboxCourierID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtboxCourierdate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
     }
 }
